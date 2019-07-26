432d4d5090ceb6e00409dd0ccf207bfa681db432
Viven <johndoetestexample2018@gmail.com>
2019-07-26 20:38:55 +0300

    2

--- Test.php
+++ Test.php
@@ -8,15 +8,43 @@ error_reporting(E_ALL);
 //foreach ($arr as $v) {
 //    echo $v;
 //}
-print("<br><hr>");
-$prn0 = new Employee();
-echo $prn0;
-$prn = new Employee("JACK");
-$prn2 = new Employee("Bob");
-$prn->setSalary(11000);
-$prn2->setSalary(22333);
-$prn->department = "new dep";
-$prn2->department = "bookkeeping dep";
-echo $prn;
-echo $prn2;
+//print("<br><hr>");
+//$prn0 = new Employee();
+//echo $prn0;
+//$prn = new Employee("JACK");
+//$prn2 = new Employee("Bob");
+//$prn->setSalary(11000);
+//$prn2->setSalary(22333);
+//$prn->department = "new dep";
+//$prn2->department = "bookkeeping dep";
+//echo $prn;
+//echo $prn2;
+
+/*class A {
+    public $x;
+
+    function example() {
+        $this->x= "foo";
+        echo  "I am A::example() and provide basic functionality.<br />\n";
+    }
+}
+
+class B extends A {
+    function example() {
+        echo "I am B::example() and provide additional functionality.<br />\n";
+        parent::example();
+    }
+}
+
+$b = new B;
+
+// This will call B::example(), which will in turn call A::example().
+$b->example();
+echo $b->x;*/
+
+$ar = "super long that";
+
+$ss = list($a , $b, $c) = explode(" ",$ar );
+var_dump($a);
+
 
